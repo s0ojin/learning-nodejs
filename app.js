258b0367bda17a9bbe16bcc4c4e990ffb82b3323
soojin <onepst@hanyang.ac.kr>
2024-09-05 22:57:54 +0900

    view분리

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
 const express = require("express");
 const app = express();
 
+// app setting
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("여기는 루트입니다. ");
+  res.render("home/index");
 });
 
 app.get("/login", (req, res) => {
-  res.send(`<!DOCTYPE html>
-<html lang="ko">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-  </head>
-  <body>
-    <input type="text" placeholder="로그인" /><br/>
-    <input type="text" placeholder="비밀번호"/><br/>
-    <button>로그인</button>
-  </body>
-</html>
-`);
+  res.render("home/login");
 });
 
 app.listen(3000, () => {
