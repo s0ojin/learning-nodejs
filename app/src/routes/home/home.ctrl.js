2475c13bf958b318f14040a4c4904fa489ddbda9
soojin <onepst@hanyang.ac.kr>
2024-09-21 23:59:36 +0900

    UserStorage 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const UserStorage = require("../../models/UserStorage");
+
 const view = {
   hello: (req, res) => {
     res.render("home/index");
@@ -9,29 +11,25 @@ const view = {
   },
 };
 
-const users = {
-  id: ["sujin", "hongdoo"],
-  psword: ["1234", "123456"],
-};
-
 const process = {
   login: (req, res) => {
     const reqId = req.body.id,
       reqPsword = req.body.psword;
 
+    const users = UserStorage.getUsers("id", "psword");
+
+    const response = {};
     if (users.id.includes(reqId)) {
       const idx = users.id.indexOf(reqId);
       if (users.psword[idx] === reqPsword) {
-        return res.json({
-          success: true,
-        });
+        response.success = true;
+        return res.json(response);
       }
     }
 
-    return res.json({
-      success: false,
-      msg: "로그인에 실패했습니다.",
-    });
+    response.success = false;
+    response.msg = "로그인에 실패했습니다.";
+    return res.json(response);
   },
 };
 
